3d920e9b9f96c7f974c13db41ccc6a283b6136f2
aedank0 <aedank@umich.edu>
2023-03-10 02:05:03 -0500

    Add the rest of basic tests

--- testing.cpp
+++ testing.cpp
@@ -4,6 +4,7 @@
 
 #include <limits>
 #include <cstdint>
+#include <type_traits>
 
 #include "gtest/gtest.h"
 
@@ -22,4 +23,32 @@ TEST(BasicHashTest, IntegralHash)
     EXPECT_NE(cb::HashThis(std::numeric_limits<std::uint64_t>::max()), cb::HashThis(std::uint64_t(1)));
     EXPECT_NE(cb::HashThis(std::numeric_limits<std::uint32_t>::max()), cb::HashThis(std::uint32_t(0)));
     EXPECT_NE(cb::HashThis(std::numeric_limits<std::uint32_t>::max()), cb::HashThis(std::uint32_t(1)));
+}
+
+TEST(BasicHashTest, FloatHash)
+{
+    EXPECT_NE(cb::HashThis(0.0f), cb::HashThis(1.0f));
+    EXPECT_NE(cb::HashThis(1.0f), cb::HashThis(2.0f));
+    EXPECT_NE(cb::HashThis(-1.0f), cb::HashThis(1.0f));
+    EXPECT_NE(cb::HashThis(0.0), cb::HashThis(1.0));
+    EXPECT_NE(cb::HashThis(1.0), cb::HashThis(2.0));
+    EXPECT_NE(cb::HashThis(-1.0), cb::HashThis(1.0));
+}
+
+struct TestStruct
+{
+    char datas[5];
+};
+
+static_assert(std::is_trivially_copyable_v<TestStruct>, "Test struct must be trivially copyable");
+static_assert(sizeof(TestStruct) == 5, "Sanity check for struct size failed");
+
+TEST(BasicHashTest, TrivialHash)
+{
+    TestStruct a = { { 1, 2, 3, 4, 5 } };
+    TestStruct b = { { 2, 3, 4, 5, 6 } };
+
+    auto aHash = cb::HashThis(a), bHash = cb::HashThis(b);
+
+    EXPECT_NE(aHash, bHash) << aHash << " != " << bHash;
 }
